5c8734c012e83c5816daac0436b7b24d1b0c54e8
Muqi Li <muqili@Muqis-MacBook-Pro.local>
2018-09-20 15:45:11 -0700

    v2

--- onlineStockSpan.cpp
+++ onlineStockSpan.cpp
@@ -1,6 +1,7 @@
 /*
 901. Online Stock Span
 https://leetcode.com/problems/online-stock-span/description/
+https://www.youtube.com/watch?v=RGRC46zHB98&list=PLLuMmzMTgVK7vEbeHBDD42pqqG36jhuOr
 Write a class StockSpanner which collects daily price quotes for some stock, and returns the span of that stock's price for the current day.
 
 The span of the stock's price today is defined as the maximum number of consecutive days (starting from today and going backwards) for which the price of the stock was less than or equal to today's price.
