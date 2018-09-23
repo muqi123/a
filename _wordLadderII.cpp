05cd996f89da887bff7ea03d301effd91f08d0ad
muqi <muqili@microsoft.com>
2018-09-23 13:14:38 -0700

    wordlistII

--- _wordLadderII.cpp
+++ _wordLadderII.cpp
@@ -12,81 +12,82 @@ All words have the same length.
 All words contain only lowercase alphabetic characters.
 You may assume no duplicates in the word list.
 You may assume beginWord and endWord are non-empty and are not the same.
-
+第二次做，要分成已经记录过，和没有记录过两种，否则会有重复。 记录过之后就把这个词从wordlist当中删掉
 */
+
 
 
 class Solution {
 public:
     vector<vector<string>> findLadders(string beginWord, string endWord, vector<string>& wordList) {
-        if( beginWord == endWord )
-            return vector<vector<string>>( 1, {beginWord});
-        unordered_map<string, int> strStep; // min step for the world
-        unordered_map<string, set<string>> p; // word parent
-        unordered_set<string> st( wordList.begin(), wordList.end());
+        unordered_set<string> st( wordList.begin(), wordList.end() );
         st.erase( beginWord );
-        bool isLast = false;
-        int step = 0;
-        queue<string> q1;
-        q1.push( beginWord);
-        while( !q1.empty() & !isLast )
-        {
-            queue<string> q2;
-            while( !q1.empty() )
-            {
-                string cur = q1.front();
-                q1.pop();
-                string cur1 = cur;
-                for( int i = 0; i < cur.size(); i++ )
-                    for( int j = 0; j < 26; j++ )
-                    {
-                        cur[i] = 'a'+j;
-                        if( cur == endWord && st.count( cur ) )
-                        {
-                            isLast = true;
-                            p[cur].insert( cur1 );
-                        }
-                        else
-                        {
-                            if( st.count(cur) && ( strStep.count( cur) == 0 || strStep[cur] == step ) )
-                            {
-                                strStep[cur] = step;
-                                p[cur].insert( cur1 );
-                                q2.push( cur );
-                            }
-                        }
-                        
-                        cur = cur1;
-                        
-                    }
-            }
-            step++;
-            q1.swap(q2);
-        }
+        unordered_map<string, int> mp;
+        unordered_map<string, vector<string>> pa;
         
         vector<vector<string>> res;
-        if( !isLast )
-            return res;
-        vector<string> vec(1, endWord );
-        dfs( p, endWord, vec, beginWord, res);
         
-        for( auto& elem : res )
-            reverse( elem.begin(), elem.end() );
+        queue<string> q;
+        q.push( beginWord );
+        mp[beginWord] = 0;
+        int step = 1;
+        bool isFound = false;
+        while( !q.empty() )
+        {
+            int sz = q.size();
+            while( sz-- )
+            {
+                
+                string c = q.front();
+                q.pop();
+                for( int i = 0; i < c.size(); i++ )
+                {
+                    string tc = c;
+                    for( int j = 0; j < 26; j++ )
+                    {
+                        tc[i] = 'a'+ j;
+                        // Not a new word
+                        if( mp.count(tc) != 0 && mp[tc] == step )
+                        {
+                            pa[tc].push_back( c );
+                            continue;
+                        }
+                        
+                        // New word
+                        if( tc == c || st.count(tc) == 0 || mp.count(tc) != 0 )
+                            continue;
+                        pa[tc].push_back( c );
+                        q.push( tc );
+                        mp[tc] = step;
+                        st.erase( tc );
+                        
+                        if( tc == endWord )
+                            isFound = true;
+                    }
+                }
+            }
+            step++;
+            if( isFound == true )
+                break;
+        }
+        vector<string> vec;
+        if( isFound )
+            findPa( pa, endWord, vec, res);
         return res;
+        
     }
     
-    void dfs( unordered_map<string, set<string>>& p, string cur, vector<string>& vec, string beginWord, vector<vector<string>>& res )
+    void findPa( unordered_map<string, vector<string>>& pa, string word, vector<string>& vec, vector<vector<string>>& res )
     {
-        if( cur == beginWord )
-        {
+        if( pa.count( word ) == 0 ){
             res.push_back( vec );
+            res.back().push_back( word );
+            reverse( res.back().begin(), res.back().end() );
             return;
         }
-        for( auto str : p[cur] )
-        {
-            vec.push_back( str );
-            dfs( p, str, vec, beginWord, res );
-            vec.pop_back();
-        }
+        vec.push_back( word );
+        for( auto elem : pa[word] )
+            findPa( pa, elem, vec, res );
+        vec.pop_back();
     }
 };
