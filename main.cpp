06cfc427576b783e2e1fe4d67d3fb7e284a14c85
muqi <muqili@microsoft.com>
2019-05-06 22:43:56 -0700

    way to compile c++

--- main.cpp
+++ main.cpp
@@ -1,9 +1,11 @@
 #include "pre.h"
-
+// use code runner 
+// Ctrl+Alt+N or F1 + run code
 int main(){
     int a = 100;
     int b = 20;
     a += b;
+    b += 1;
     cout<<"Hellow World"<<endl;
     cout<< a << endl;
     return 0; 
